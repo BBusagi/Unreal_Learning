a4d9aa66d723b90a899554f41b4bb092bda54c14
Riku <lz354000575@gmail.com>
2024-08-15 17:34:03 +0900

    P89 finished

--- Project3_CR/Source/Project3_CR/Grabber.cpp
+++ Project3_CR/Source/Project3_CR/Grabber.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Grabber.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -25,13 +27,40 @@ void UGrabber::BeginPlay()
 
 
 // Called every frame
+// void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+// {
+// 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+// 	FRotator MyRotation = GetComponentRotation();
+// 	FString RotationString = MyRotation.ToCompactString();
+// 	UE_LOG(LogTemp, Display, TEXT("Grabber Rotation: %s"), *RotationString);
+
+// 	float Time = GetWorld() ->TimeSeconds;
+// 	UE_LOG(LogTemp, Display, TEXT("Cuerrent time is %f"), Time);
+// 	// ...
+// }
+
+//P87
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	FRotator MyRotation = GetComponentRotation();
-	FString RotationString = MyRotation.ToCompactString();
-	UE_LOG(LogTemp, Display, TEXT("Grabber Rotation: %s"), *RotationString);
-	// ...
+	FVector Start = GetComponentLocation();
+	FVector End = Start + GetForwardVector() * MaxGrabDistance;
+	DrawDebugLine(GetWorld(),Start,End, FColor::Red);
+
+	float Damage = 0;
+	// float& DamageRef = Damage;
+	// DamageRef = 5;
+	//UE_LOG(LogTemp, Display, TEXT("Damage: %f/%f"),DamageRef,Damage);
+	
+	UE_LOG(LogTemp, Display, TEXT("Original  Damage: %f"), Damage);
+	PrintDamage(Damage);
+
 }
 
+void UGrabber::PrintDamage(const float& Damage)
+{
+	//Damage = 2; 
+	UE_LOG(LogTemp, Display, TEXT("Damage: %f"), Damage);
+}
