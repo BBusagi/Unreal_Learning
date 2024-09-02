c4ed230938faa0d0831c7aaaf63b654a864ca58c
RikusyuN <zhoul@nangokrstudios.jp>
2024-09-02 10:14:41 +0900

    P102 finished

--- Project3_CR/Source/Project3_CR/Trigger.cpp
+++ Project3_CR/Source/Project3_CR/Trigger.cpp
@@ -21,6 +21,29 @@ void UTrigger::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-    UE_LOG(LogTemp, Display, TEXT("Trigger Component is Ticking"));
+    
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
 
+    // Method 1
+    // int32 index = 0;
+    // while (index < Actors.Num())
+    // {
+    //     FString ActorName = Actors[index]->GetActorNameOrLabel();
+    //     UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+    //     ++index;
+    // }
+    
+    //Method 2
+    // for (int i = 0; i < Actors.Num(); i++)
+    // {
+    //     FString ActorName = Actors[i]->GetActorNameOrLabel();
+    //     UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+    // }
+    
+    for(AActor* Actor : Actors)
+    {
+        FString ActorName = Actor->GetActorNameOrLabel();
+        UE_LOG(LogTemp, Display, TEXT("Overlapping: %s"), *ActorName);
+    }
 }
